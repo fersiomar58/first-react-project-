b1ccda9db1e716f60a7161b860c77f852d138f74
fersiomar58 <97899733+fersiomar58@users.noreply.github.com>
2022-02-24 13:47:44 +0100

    my first react project

---
+++ src/App.js
@@ -0,0 +1,20 @@
+
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Button } from 'react-bootstrap';
+import Forme from './components/Form';
+
+
+
+
+
+function App() {
+  return (
+    <div style={{backgroundColor: "teal"}} className="App">
+      <h1 style={{color : 'bleu'}}>Welcome To our Fondation</h1>
+<Forme/>
+    </div>
+  );
+}
+
+export default App;
